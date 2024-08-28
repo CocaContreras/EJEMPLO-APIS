97f6db9a5f98a2c2d8671b4cd9ea948ccc702545
CocaContreras <oc26686@gmail.com>
2024-08-27 23:25:21 -0600

    actudb2

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'umg_2024_11854_zce4',
     username: 'umg_2024_11854_zce4_user',
     password: 'qytmb5Lc19O34U0FoCtW2qCXB3s0Wj4f',
-    host: 'dpg-cr6jnrlds78s73burqhg-a',
+    host: 'dpg-cr6jnrlds78s73burqhg-a.oregon-postgres.render.com',
     dialect: 'postgres',
     pool: {
       max: 5,
